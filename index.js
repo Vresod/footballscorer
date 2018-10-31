ecff18b9cfa4c5bf1494c93c5ca10aceb49c1f56
Vresod <40863375+Vresod@users.noreply.github.com>
2018-10-30 21:29:35 -0400

    added stuff

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function scorefunc2(x) {
 	team2 = team2 + x;
 	document.getElementById("score2").innerHTML = team2;
 }
+
+function change() {
+	name1 = prompt('What is the name for team one?');
+	name2 = prompt('What is the name for team two?');
+	document.getElementById('name1').innerHTML = name1;
+	document.getElementById('name2').innerHTML = name2;
+}
+
+function reset() {
+	document.getElementById('name1').innerHTML = "One";
+	document.getElementById('name2').innerHTML = "Two";
+	team1 = 0;
+	team2 = 0;
+}
